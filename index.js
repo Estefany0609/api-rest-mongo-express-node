793fae895e54c383f51c2432b99f78648a74f5f5
Estefany0609 <estefanymeleon@hotmail.com>
2023-08-29 13:20:33 -0500

    ajusteListaBlanca

--- index.js
+++ index.js
@@ -7,19 +7,34 @@ import cookieParser from "cookie-parser";
 import simulatorRouter from "./routes/simulator.route.js";
 import generalRouter from "./routes/general.route.js";
 import dataRouter from "./routes/data.route.js";
-//import redirectRouter from "./routes/redirect.route.js";
 
 const app = express();
 
-const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
+const whiteList = [
+  /^https?:\/\/localhost(:\d+)?$/, // Coincide con cualquier puerto en localhost
+  "api-rest-mongo-express-node-production.up.railway.app",
+];
+
+const checkWhiteList = (origin) => {
+  return whiteList.some((allowedOrigin) => {
+    if (typeof allowedOrigin === "string") {
+      return origin === allowedOrigin;
+    } else if (allowedOrigin instanceof RegExp) {
+      return allowedOrigin.test(origin);
+    }
+    return false;
+  });
+};
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || whiteList.includes(origin)) {
+      if (!origin || checkWhiteList(origin)) {
         return callback(null, true);
       }
-      return callback("Error de CORS origin: " + origin + " No autorizado");
+      return callback(
+        new Error("Error de CORS origin: " + origin + " No autorizado")
+      );
     },
     credentials: true,
   })
@@ -28,16 +43,10 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-//Ejemplo BackRedirect (opcional)
-//app.use("/", redirectRouter)
-
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/position", simulatorRouter);
 app.use("/api/v1/general", generalRouter);
 app.use("/api/v1/data", dataRouter);
-
-// Solo para el ejemplo de login y token
-//app.use(express.static("public"))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("🔥🔥🔥🔥 http://localhost:" + PORT));
